77d9376d2aa993a1878ccb387b37837bd6c28b50
Erin Shaben <eshaben@icloud.com>
2022-07-01 14:49:50 -0400

    update squid to latest based off of template

--- src/processor.ts
+++ src/processor.ts
@@ -1,46 +1,78 @@
-import {
-  EventHandlerContext,
-  SubstrateProcessor,
-} from "@subsquid/substrate-processor";
-import { lookupArchive } from "@subsquid/archive-registry";
+import {lookupArchive} from "@subsquid/archive-registry"
+import * as ss58 from "@subsquid/ss58"
+import {BatchContext, BatchProcessorItem, SubstrateBatchProcessor} from "@subsquid/substrate-processor"
+import {Store, TypeormDatabase} from "@subsquid/typeorm-store"
+import {In} from "typeorm"
+import {BalancesTransferEvent} from "./types/events"
 import { Transfer } from "./model";
-import { BalancesTransferEvent } from "./types/events";
 
-const processor = new SubstrateProcessor("moonriver-balance-transfers");
 
-processor.setBatchSize(500);
-processor.setDataSource({
-  archive: lookupArchive("moonriver")[0].url,
-  chain: "wss://moonriver.api.onfinality.io/public-ws",
-});
-processor.setBlockRange({from: 0})
+const processor = new SubstrateBatchProcessor()
+    .setBatchSize(500)
+    .setDataSource({
+        // Lookup archive by the network name in the Subsquid registry
+        archive: lookupArchive("moonriver", { release: "FireSquid" })
 
-processor.addEventHandler("balances.Transfer", async (ctx: EventHandlerContext) => {
-  const event = getTransferEvent(ctx);
+        // Use archive created by archive/docker-compose.yml
+        // archive: 'http://localhost:8888/graphql'
+    })
+    .addEvent('Balances.Transfer', {
+        data: {event: {args: true}}
+    } as const)
 
-  
-  const transferred = new Transfer();
-  transferred.id = ctx.event.id;
-  transferred.balance = event.amount;
-  transferred.from = ctx.event.params[1].value as string;
-  transferred.to = ctx.event.params[2].value as string;
 
-  // // only save a transfer if the to or from is one of the WGLMR contracts
-  const WMOVR = ["0x98878B06940aE243284CA214f92Bb71a2b032B8A", "0xE3C7487Eb01C74b73B7184D198c7fBF46b34E5AF", "0xf50225a84382c74CbdeA10b0c176f71fc3DE0C4d"]
-  if (WMOVR.includes(transferred.from) || WMOVR.includes(transferred.to)){
-    await ctx.store.save(transferred);
-  }
-});
+type Item = BatchProcessorItem<typeof processor>
+type Ctx = BatchContext<Store, Item>
 
-function getTransferEvent(ctx: EventHandlerContext) {
-  const event = new BalancesTransferEvent(ctx);
 
-  if (event.isV49) {
-    const [from, to, value] = event.asV49;
-    return {from, to, amount: value};
-  } else {
-    return event.asV1201
-  }
+processor.run(new TypeormDatabase(), async ctx => {
+    let transfers = getTransfers(ctx)
+
+    for (let t of transfers) {
+      const transferred = new Transfer();
+      transferred.id = t.id
+      transferred.balance = t.balance;
+      transferred.from = t.from
+      transferred.to = t.to;
+
+      // // only save a transfer if the to or from is one of the WGLMR contracts
+      const WMOVR = ["0x98878B06940aE243284CA214f92Bb71a2b032B8A", "0xE3C7487Eb01C74b73B7184D198c7fBF46b34E5AF", "0xf50225a84382c74CbdeA10b0c176f71fc3DE0C4d"]
+      if (WMOVR.includes(transferred.from) || WMOVR.includes(transferred.to)){
+        await ctx.store.save(transferred);
+      }
+    }
+})
+
+
+interface TransferEvent {
+    id: string
+    from: string
+    to: string
+    balance: bigint
 }
 
-processor.run();
+
+function getTransfers(ctx: Ctx): TransferEvent[] {
+    let transfers: TransferEvent[] = []
+    for (let block of ctx.blocks) {
+        for (let item of block.items) {
+            if (item.name == "Balances.Transfer") {
+                let e = new BalancesTransferEvent(ctx, item.event)
+                let rec: {from: Uint8Array, to: Uint8Array, amount: bigint}
+                if (e.isV49) {
+                    let [from, to, value ] = e.asV49
+                    rec = {from, to, amount: value}
+                } else {
+                    rec = e.asV1201
+                }
+                transfers.push({
+                    id: item.event.id,
+                    from: item.event.args[0],
+                    to: item.event.args[1],
+                    balance: item.event.args[2],
+                })
+            }
+        }
+    }
+    return transfers
+}
--- src/types/events.ts
+++ src/types/events.ts
@@ -1,32 +1,41 @@
 import assert from 'assert'
-import {EventContext, Result, deprecateLatest} from './support'
+import {Chain, ChainContext, EventContext, Event, Result} from './support'
+import * as v49 from './v49'
 import * as v1201 from './v1201'
 
 export class BalancesTransferEvent {
-  constructor(private ctx: EventContext) {
-    assert(this.ctx.event.name === 'balances.Transfer')
+  private readonly _chain: Chain
+  private readonly event: Event
+
+  constructor(ctx: EventContext)
+  constructor(ctx: ChainContext, event: Event)
+  constructor(ctx: EventContext, event?: Event) {
+    event = event || ctx.event
+    assert(event.name === 'Balances.Transfer')
+    this._chain = ctx._chain
+    this.event = event
   }
 
   /**
    *  Transfer succeeded. \[from, to, value\]
    */
   get isV49(): boolean {
-    return this.ctx._chain.getEventHash('balances.Transfer') === 'dfcae516f053c47e7cb49e0718f01587efcb64cea4e3baf4c6973a29891f7841'
+    return this._chain.getEventHash('Balances.Transfer') === 'dfcae516f053c47e7cb49e0718f01587efcb64cea4e3baf4c6973a29891f7841'
   }
 
   /**
    *  Transfer succeeded. \[from, to, value\]
    */
-  get asV49(): [Uint8Array, Uint8Array, bigint] {
+  get asV49(): [v49.AccountId, v49.AccountId, v49.Balance] {
     assert(this.isV49)
-    return this.ctx._chain.decodeEvent(this.ctx.event)
+    return this._chain.decodeEvent(this.event)
   }
 
   /**
    * Transfer succeeded.
    */
   get isV1201(): boolean {
-    return this.ctx._chain.getEventHash('balances.Transfer') === '23222c59f2992c12387568241620899d2d399ab9027595daca8255637f62ece3'
+    return this._chain.getEventHash('Balances.Transfer') === '23222c59f2992c12387568241620899d2d399ab9027595daca8255637f62ece3'
   }
 
   /**
@@ -34,16 +43,6 @@ export class BalancesTransferEvent {
    */
   get asV1201(): {from: v1201.AccountId20, to: v1201.AccountId20, amount: bigint} {
     assert(this.isV1201)
-    return this.ctx._chain.decodeEvent(this.ctx.event)
-  }
-
-  get isLatest(): boolean {
-    deprecateLatest()
-    return this.isV1201
-  }
-
-  get asLatest(): {from: v1201.AccountId20, to: v1201.AccountId20, amount: bigint} {
-    deprecateLatest()
-    return this.asV1201
+    return this._chain.decodeEvent(this.event)
   }
 }
